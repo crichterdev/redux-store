031d70d288bc64b3da6f2977e3efa0e6f5432925
Carlos Richter <carlos.richter.dev@outlook.com>
2024-01-16 23:18:34 -0400

    01-store-creation-initial-state - View-only

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import { renderTodos } from './utils';
+import * as fromStore from "./store"; //folder
+import { renderTodos } from "./utils";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
+
+const store = new fromStore.Store({},
+{
+ todos:[{label:'Eat pizza', complete: false}],
+});
 
 button.addEventListener(
-  'click',
+  "click",
   () => {
     if (!input.value.trim()) return;
 
@@ -14,14 +20,14 @@ button.addEventListener(
 
     console.log(payload);
 
-    input.value = '';
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener("click", function (event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
+  if (target.nodeName.toLowerCase() === "button") {
     console.log(target);
   }
 });
